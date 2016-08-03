cfbdda8fb1361f451329e7d2e8ea3001488f1ca3
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2016-08-03 11:22:48 +0200

    Improve graphics
    - Add bootstrap CSS
    - Move logo to page bottom
    - Cosmetic changes for tables and forms

--- phphtdocs/footer.php
+++ phphtdocs/footer.php
@@ -1,3 +1,8 @@
-<?
-echo("Page load completed in ". (time() - $starttime) ." seconds"); 
-?>
+
+<div class='containter-fluid' style='text-align: center; margin-top: 20px'>
+<a href='http://bandwidthd.sourceforge.net/'><img src="logo.gif" style="width:200px; height: auto;"></a>
+</div>
+
+</div> <!--- container -->
+</body>
+</html>
